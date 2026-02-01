2f43994d09260297001c6e5e04f73be558a5fccd
dragon-r <d.rak60@outlook.com>
2026-02-01 18:41:07 +0100

    Added Cart Component
    Parent component to all other cart components! Will be updated with state and extra child components

---
+++ src/Components/Header/Cart/Cart.js
@@ -0,0 +1,13 @@
+import './_cart.scss';
+
+import CartButton from './CartButton/CartButton';
+
+function Cart() {
+    return (
+        <div className='cart'>
+            <CartButton />
+        </div>
+    )
+}
+
+export default Cart;
