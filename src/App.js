2323ac4fb937519c67fa5115c4c983e038a3aa2c
dragon-r <d.rak60@outlook.com>
2026-02-03 20:26:40 +0100

    Added initial Store parent component
    Included it in App. Will work on it in the future

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './Components/Header/Header';
 import Logo from './Components/Header/Logo/Logo';
 import Cart from './Components/Header/Cart/Cart';
 
+import Store from './Components/Store/Store';
+
 import Footer from './Components/Footer/Footer';
 
 export function App() {
@@ -11,6 +13,7 @@ export function App() {
         <Logo brandname="TechStore" />
         <Cart />
       </Header>
+      <Store />
       <Footer />
     </>
   );
---
+++ src/Components/Store/Store.js
@@ -0,0 +1,20 @@
+import './_store.scss';
+import {useState, useEffect} from 'react';
+
+import StoreHeading from './StoreHeading/StoreHeading';
+
+function Store() {
+    const [products, setProducts] = useState(false);
+    const [filters, setFilters] = useState({});
+    const [page, setPage] = useState(false);
+    return (
+        <section className='store container'>
+            <StoreHeading 
+                main="Discover Amazing Tech Products"
+                sub="Browse our collection of 60 premium tech products from top brands"
+            />
+        </section>
+    )
+}
+
+export default Store;
