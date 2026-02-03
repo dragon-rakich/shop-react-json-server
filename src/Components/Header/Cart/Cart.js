6d459f26857a59dbcc0b21765c97bc17be3f2554
dragon-r <d.rak60@outlook.com>
2026-02-03 17:44:29 +0100

    Added api to Cart

--- src/Components/Header/Cart/Cart.js
+++ src/Components/Header/Cart/Cart.js
@@ -13,12 +13,60 @@ function Cart() {
     const toggleCart = (e) => {
         setIsShowing(prev => !prev);
     }
+
+    const updateCart = (cartItem, operation) => {
+        let api = "http://localhost:3000/cart/" + cartItem.id;
+        if (operation === "increase") {
+            fetch(api, {method: "PATCH", body: JSON.stringify({count: Number(cartItem.count) + 1}), headers: {"Content-Type": "application/json"}});
+        }
+        else if (operation === "decrease") {
+            if (Number(cartItem.count) === 1) {return;}
+            fetch(api, {method: "PATCH", body: JSON.stringify({count: Number(cartItem.count) - 1}), headers: {"Content-Type": "application/json"}});
+        }
+        else if (operation === "delete") {
+            fetch(api, {method: "DELETE"});
+        }
+    }
+
+    useEffect(() => {
+        console.log("boop");
+        let timeoutId;
+
+        async function poll() {
+            try {
+                const response = await fetch("http://localhost:3000/cart", {method: "GET"});
+                const data = await response.json();
+                console.log("New data:", data);
+                setCart(data);
+                timeoutId = setTimeout(poll, 1000);
+            } catch (err) {
+                console.log("Polling error", err);
+                //timeoutId = setTimeout(poll, 3000);
+            }
+        }
+
+        poll();
+
+        return () => {
+            console.log("Stopping polling...");
+            clearTimeout(timeoutId);
+        };
+    }, []); 
+
+
+
+
     return (
         <div className='cart'>
             <CartButton onClick={toggleCart} />
             {(cart && cart.length > 0) && <CartCount cart={cart} />}
             {(isShowing) && <CartOverlay onClick={toggleCart} />}
-            <CartPanel cart={cart} />
+            <CartPanel 
+                cart={cart} 
+                isShowing={isShowing}
+                onHide={toggleCart}
+                onUpdate={updateCart}
+            />
         </div>
     )
 }
