615982c7bb2fda501e3f13fccb540f558889ba77
dragon-r <d.rak60@outlook.com>
2026-02-03 16:28:54 +0100

    Updated calculating count in CartCount

--- src/Components/Header/Cart/CartCount/CartCount.js
+++ src/Components/Header/Cart/CartCount/CartCount.js
@@ -1,6 +1,7 @@
 import './_cart-count.scss';
 
-function CartCount({count}) {
+function CartCount({cart}) {
+    const count = cart.reduce((sum, cartItem) => sum + (Number(cartItem.count)), 0);
     return (
         <p className="cart-count">{count}</p>
     )
