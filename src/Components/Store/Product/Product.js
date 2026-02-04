4dcf5e124d6854ea57a411ebef6ded802e93bb5a
dragon-r <d.rak60@outlook.com>
2026-02-04 14:30:50 +0100

    Added Product.js

---
+++ src/Components/Store/Product/Product.js
@@ -0,0 +1,22 @@
+import './_product.scss';
+
+function Product({product}, onBuy) {
+    return (
+        <article className='product'>
+            <div className='product__image' />
+            <div className='product__ui'>
+                <h2 className='product__name'>{product.name}</h2>
+                <p className='product__line'>{product.description}</p>
+                <p className='product__line'>{product.brand} • {product.category}</p>
+                <div className='product__rating-box'>
+                    <div className='product__rating-star' />
+                    <p className='product__rating-num'>{product.rating}</p>
+                </div>
+                <p>${product.price}</p>
+                <button onClick={onBuy}>Add to Cart</button>
+            </div>
+        </article>
+    )
+}
+
+export default Product;
