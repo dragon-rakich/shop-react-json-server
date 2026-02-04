fb091b608b1acb174752056d4dfc3f9e9a783a42
dragon-r <d.rak60@outlook.com>
2026-02-04 21:43:28 +0100

    Completed full Button component

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './_btn.scss';
+
+function Button({label, type, size, onClick, isSubmit, isDisabled}) {
+    let className = "btn" + " btn--" + type + " btn--" + size;    
+
+    const handleClick = (e) => {
+        console.log("Button click");
+        if (typeof(onClick) === "function") {
+            onClick();
+        }
+    }
+    return (
+        <button onClick={handleClick} disabled={isDisabled} className={className} type={(isSubmit) ? "submit":"button"}>{label}</button>
+    )
+}
+
+export default Button;
