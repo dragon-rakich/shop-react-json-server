4ca374187200cbea2aee2afc89605aef4a7a3710
dragon-r <d.rak60@outlook.com>
2026-02-07 13:18:14 +0100

    Added type to InputField

--- src/Components/Store/Filters/InputField/InputField.js
+++ src/Components/Store/Filters/InputField/InputField.js
@@ -1,12 +1,12 @@
 import './_input-field.scss';
 
-function InputField({value, onChange, placeholder}) {
+function InputField({type, value, onChange, placeholder}) {
     const handleChange = (e) => {
         if (typeof(onChange) === "function") {
             onChange(e.target.value);
         }
     }
-    return <input className='input-field' onChange={handleChange} value={value} placeholder={placeholder} />
+    return <input className='input-field' onChange={handleChange} value={value} placeholder={placeholder} type={type} />
 }
 
 export default InputField;
