184037d2858d773207c30ad1907073b1f1b4ac85
dragon-r <d.rak60@outlook.com>
2026-02-07 12:49:27 +0100

    Added full Input component

---
+++ src/Components/Store/Filters/InputField/InputField.js
@@ -0,0 +1,12 @@
+import './_input-field.scss';
+
+function InputField({value, onChange, placeholder}) {
+    const handleChange = (e) => {
+        if (typeof(onChange) === "function") {
+            onChange(e.target.value);
+        }
+    }
+    return <input className='input-field' onChange={handleChange} value={value} placeholder={placeholder} />
+}
+
+export default InputField;
