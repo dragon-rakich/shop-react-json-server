e7b4c6708799e816ee6fd06aadec88da276ce91d
dragon-r <d.rak60@outlook.com>
2026-02-09 11:48:06 +0100

    Updated Dropdown
    Updated onChange event handler, how the selected value label is found and rendered. Updated  styles, included check icon(must find better one)

--- src/Components/Store/Filters/Dropdown/Dropdown.js
+++ src/Components/Store/Filters/Dropdown/Dropdown.js
@@ -6,24 +6,24 @@ function Dropdown({value, options, onChange}) {
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = (e) => {setIsOpen(prev => !prev)}
-    const handleChange = (e) => {
+    const handleChange = (newValue) => {
         if (typeof(onChange) === "function") {
-            onChange(e.target.value);
+            onChange(newValue);
             toggleOpen();
         }
     }
 
     return (
         <div className='dropdown'>
-            <div className='dropdown__value' onClick={toggleOpen}>{value}</div>
+            <div className='dropdown__value' onClick={toggleOpen}>{options.find(option => String(option.value) === String(value)).label}</div> {/*check out option JSON.stringify(arr1) === JSON.stringify(arr2) */}
 
             {(isOpen) && 
             <ul className='dropdown__menu'>
                 {options.map((option, i) => {
                     let className = "dropdown__option";
-                    if (option === value) {className += " dropdown__option--selected"}
+                    if (option.value === value) {className += " dropdown__option--selected"}
 
-                    return <li key={i} className={className} onClick={handleChange} value={value}>{option}</li>
+                    return <li key={i} className={className} onClick={() => {handleChange(option.value)}} value={option.value}>{option.label}</li>
                 }
                 )}
             
