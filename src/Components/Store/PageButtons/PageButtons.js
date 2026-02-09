8f3ed60216a34db1961d437cf4d251e54d02157c
dragon-r <d.rak60@outlook.com>
2026-02-09 19:35:24 +0100

    Added Full PageButtons

---
+++ src/Components/Store/PageButtons/PageButtons.js
@@ -0,0 +1,36 @@
+import './_page-buttons.scss';
+
+import Button from '../../Button/Button';
+
+function PageButtons({numOfPages, page, onClick}) {
+    return (
+        <div className='page-buttons'>
+            <Button 
+                label=" < "
+                type="primary"
+                size="medium"
+                onClick={() => {onClick(page - 1)}}
+                isDisabled={(page === 1) ? true:false}
+            />
+            <div className='page-buttons__numbers'>
+                {Array.from({ length: numOfPages }, (_, i) => i + 1).map((num) => 
+                    <Button
+                        label={num}
+                        type={(num !== page) ? "primary":"secondary"}
+                        size="medium"
+                        onClick={() => {onClick(num)}}
+                    />
+                )}
+            </div>
+            <Button 
+                label=" > "
+                type="primary"
+                size="medium"
+                onClick={() => {onClick(page + 1)}}
+                isDisabled={(page === numOfPages) ? true:false}
+            />
+        </div>
+    )
+}
+
+export default PageButtons;
