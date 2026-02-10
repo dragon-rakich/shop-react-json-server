00c6ae95e7479cd0842fcf77e12440325d761e7b
dragon-r <d.rak60@outlook.com>
2026-02-10 18:30:07 +0100

    Fixed minor issues

--- src/Components/Store/Products/NoResults/NoResults.js
+++ src/Components/Store/Products/NoResults/NoResults.js
@@ -3,8 +3,8 @@ import './_no-results.scss';
 function NoResults({title, subtitle}) {
     return (
         <div className='no-results'>
-            <p className='results__title'>{title}</p>
-            <p className='results__subtitle'>{subtitle}</p>
+            <p className='no-results__title'>{title}</p>
+            <p className='no-results__subtitle'>{subtitle}</p>
         </div>
     )
 }
