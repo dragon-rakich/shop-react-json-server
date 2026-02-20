e68e8e8d8080b5f63a38a63d2d1f404eafbc69f2
dragon-r <d.rak60@outlook.com>
2026-02-20 17:19:05 +0100

    Contact update

--- src/Components/Contact/ContactItem/ContactItem.js
+++ src/Components/Contact/ContactItem/ContactItem.js
@@ -1,9 +1,9 @@
-import '_contact-item.scss';
+import './_contact-item.scss';
 
 const icons = {
-    email : new URL("../../../../assets/icons/email.png", import.meta.url),
-    phone : new URL("../../../../assets/icons/phone.png", import.meta.url),
-    address : new URL("../../../../assets/icons/address.png", import.meta.url),
+    email : new URL("../../../assets/icons/email.png", import.meta.url),
+    phone : new URL("../../../assets/icons/phone.png", import.meta.url),
+    address : new URL("../../../assets/icons/address.png", import.meta.url),
 }
 function ContactItem({title, data}) {
     return (
@@ -11,7 +11,7 @@ function ContactItem({title, data}) {
             <img className='contact-item__icon' src={icons[title.toLowerCase()]}/>
             <div className='contact-item__data'>
                 <h2 className='contact-item__title'>{title}</h2>
-                {data.map((dataItem, i) => {<p className='contact-item__data-item' key={i}>{dataItem}</p>})}
+                {data.map((dataItem, i) => <p className='contact-item__data-item' key={i}>{dataItem}</p>)}
             </div>
         </section>
     )
