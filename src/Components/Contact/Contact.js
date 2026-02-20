5b96afade9ec550912321527af9cbea5a62cbc0b
dragon-r <d.rak60@outlook.com>
2026-02-20 15:09:34 +0100

    added full Contact component

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,12 +3,16 @@ import './_contact.scss';
 import ContactItem from './ContactItem/ContactItem';
 
 function Contact() {
-    const email = {}
+    const email = ["support@techstore.com"];
+    const phone = ["1-800-TECH-STORE", "Mon-Fri 9am-6pm EST"];
+    const address = ["123 Tech Street", "San Francisco, CA 94102"];
+
     return (
         <section className='container contact'>
+            <h1 className='contact__title'>Contact Us</h1>
             <ContactItem title="Email" data={email}/>
-            <ContactItem title="Phone" data={email}/>
-            <ContactItem title="Address" data={email}/>
+            <ContactItem title="Phone" data={phone}/>
+            <ContactItem title="Address" data={address}/>
         </section>
     )
 }
--- src/Components/Contact/ContactItem/ContactItem.js
+++ src/Components/Contact/ContactItem/ContactItem.js
@@ -1,9 +1,14 @@
 import '_contact-item.scss';
 
+const icons = {
+    email : new URL("../../../../assets/icons/email.png", import.meta.url),
+    phone : new URL("../../../../assets/icons/phone.png", import.meta.url),
+    address : new URL("../../../../assets/icons/address.png", import.meta.url),
+}
 function ContactItem({title, data}) {
     return (
         <section className='contact-item'>
-            <img className='contact-item__icon'/>
+            <img className='contact-item__icon' src={icons[title.toLowerCase()]}/>
             <div className='contact-item__data'>
                 <h2 className='contact-item__title'>{title}</h2>
                 {data.map((dataItem, i) => {<p className='contact-item__data-item' key={i}>{dataItem}</p>})}
