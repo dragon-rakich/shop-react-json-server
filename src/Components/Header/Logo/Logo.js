b2543a0d9fce1de2d0153854f6c7d42db5144ea4
dragon-r <d.rak60@outlook.com>
2026-02-21 14:48:33 +0100

    Made Logo a link to Store

--- src/Components/Header/Logo/Logo.js
+++ src/Components/Header/Logo/Logo.js
@@ -1,12 +1,13 @@
 import './_logo.scss';
+import { Link } from 'react-router-dom';
 
 function Logo({brandname, url}) {
     const logo = new URL("../../../assets/icons/store.png", import.meta.url);
     return (
-        <div className="logo">
+        <Link className="logo" to="/">
             <img className="logo__image" src={logo} alt='logo'/>
             <p className="logo__brandname">{brandname}</p>
-        </div>
+        </Link>
     )
 }
 
