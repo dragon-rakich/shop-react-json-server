a598797b69e7f41f37e7a990920be69628e198a8
dragon-r <d.rak60@outlook.com>
2026-02-21 14:53:05 +0100

    Added 2 Components/subpages to App

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Layout from './Layout';
 
 import Store from './Components/Store/Store';
 import Contact from './Components/Contact/Contact';
+import Shipping from './Components/Shipping/Shipping';
+import FAQ from './Components/FAQ/FAQ';
 
 export function App() {
   return (
@@ -17,6 +19,8 @@ export function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Store />} />
           <Route path='/contact' element={<Contact />} />
+          <Route path='/shipping' element={<Shipping />} />
+          <Route path='/faq' element={<FAQ />} />
         </Route>
       </Routes>
     </HashRouter>
